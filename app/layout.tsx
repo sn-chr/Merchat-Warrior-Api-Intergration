44967427d4a3697fea7735e545f6a09c02671041
sn-chr <rib.armor@gmail.com>
2025-02-01 15:39:50 +0900

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Merchat Warrior Api Intergration',
   description: 'Created with v0',
 }
 
